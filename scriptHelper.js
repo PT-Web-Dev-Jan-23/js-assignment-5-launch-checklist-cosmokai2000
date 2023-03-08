6b85fb39624dad836ca66ef1b034e3f43c8d4a00
Stephen Cleveland <cosmokai2000@gmail.com>
2023-03-08 13:27:51 -0600

    checking autograder

--- scriptHelper.js
+++ scriptHelper.js
@@ -42,27 +42,27 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     } else if (validateInput(pilot)===`Is a Number`||validateInput(copilot)===`Is a Number`) {
         alert('Please only enter names for the name of pilot and name of co-pilot');
     } else {
-        pilotStatus.innerHTML = `Pilot ${pilot} is ready`;
-        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready`;
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         list.style.visibility = 'hidden';
     }
     
     if (Number(fuelLevel) < 10000) {
         list.style.visibility = 'visible';
         fuelStatus.innerHTML = `Not enough fuel for journey`;
-        launchStatus.innerHTML = `Shuttle not ready for launch`;
-        launchStatus.style.color = `red`;
+        launchStatus.innerHTML = `Shuttle Not Ready for launch`;
+        launchStatus.style.color = `rgb(199, 37, 78`;
     } else if (Number(cargoLevel) > 10000) {
         list.style.visibility = `visible`;
         cargoStatus.innerHTML = `Cargo too heavy for takeoff`;
         launchStatus.innerHTML = `Shuttle not ready for launch`;
-        launchStatus.style.color = `red`;
+        launchStatus.style.color = `rgb(199, 37, 78`;
     } else if (Number(cargoLevel) < 10000 && Number(fuelLevel) > 10000) {
         list.style.visibility = `visible`;
         fuelStatus.innerHTML = `Enough fuel for journey`;
         cargoStatus.innerHTML = `Cargo light enough for takeoff`;
         launchStatus.innerHTML = `Shuttle ready for launch`;
-        launchStatus.style.color = `green`;
+        launchStatus.style.color = `rgb(65, 159, 106)`;
     }
 }
 
